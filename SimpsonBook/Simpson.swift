e89f0b6e10c157db7478bf8b4f6532d77c5565bf
Cem Eke <cem.eke14@gmail.com>
2021-01-24 12:59:12 +0300

    firstCommit

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,21 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Cem Eke on 23.01.2021.
+//
+
+import Foundation
+import SwiftUI
+
+class Simpson {
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(_name:String, _job:String, _image:UIImage) {
+        name=_name
+        job=_job
+        image=_image
+    }
+}
--- SimpsonBook/ViewController.swift
+++ SimpsonBook/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var mySimpsons = [Simpson]()
+    var chosenSimpson : Simpson?
+    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        let homer = Simpson(_name: "Homer", _job: "Öğretmen", _image: UIImage(named:"homer")!)
+        let bart = Simpson(_name: "Bart", _job: "Öğrenci", _image: UIImage(named:"bart")!)
+        let marge = Simpson(_name: "Marge", _job: "Ev Hanımı", _image: UIImage(named:"marge")!)
+        let lisa = Simpson(_name: "Lisa", _job: "Sekreter", _image: UIImage(named:"lisa")!)
+        let maggie = Simpson(_name: "Maggie", _job: "NoJob", _image: UIImage(named:"maggie")!)
+        
+        mySimpsons.append(homer)
+        mySimpsons.append(bart)
+        mySimpsons.append(marge)
+        mySimpsons.append(lisa)
+        mySimpsons.append(maggie)
+        
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mySimpsons.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = mySimpsons[indexPath.row].name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        chosenSimpson = mySimpsons[indexPath.row]
+        performSegue(withIdentifier: "todetailsVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "todetailsVC" {
+            let destinationVC = segue.destination as! detailsVC
+            destinationVC.selectedSimpson = chosenSimpson
+    
+        }
+    }
 }
 
